b0391e4c593346365b683e75d8484ce4d702e35e
DenVitaKaninen <tobiaskjernell@gmail.com>
2025-02-11 14:13:11 +0100

    added birds and started on reptiles

--- data/animal.js
+++ data/animal.js
@@ -12,8 +12,8 @@ class Animal {
 }
 
 const mammals = [new Animal('Echida',
-    '10',
-    '76',
+    '10kg',
+    '76cm',
     "Echidnas, also called spiny anteaters, are walking contradictions. They are mammals, but they lay eggs. They are often classified as long- or sort-beaked, but don't have beaks at all, in the traditional sense; they have fleshy noses that can be either on the long side or rather short.They don't really look like true anteaters (Myrmecophaga tridactyla), either, and they are not closely related to them. They are spiny, though; their bodies are covered with hollow, barbless quills. Echidnas are monotremes, egg-laying mammals. The only other living monotreme is the platypus.",
     "insects such as ants and termites, beetle larvae and worms",
     "50",
@@ -30,8 +30,8 @@ new Animal('Tasmanian Devil',
     "Mammals"
 ),
 new Animal('Quokka',
-    "3",
-    "50",
+    "3kg",
+    "50cm",
     "The Quokka, Setonix brachyurus, was described by early Dutch explorer, Willem de Vlamingh, 'as a kind of rat as big as a common cat'. His first sighting of the Quokka was on an island off the mouth of the Swan River. He named the island Rottenest ('rat nest') in honour of this sighting. The island is now known as Rottnest Island. Essentially the Quokka looks very much like other wallabies.It has short, very coarse and thick grey-brown fur over most of the body with lighter parts underneath. Its facial features consist of a naked nose on a short, broad face with rounded furry ears.The tail is relatively short and mostly devoid of hair. In contrast, the hair on the feet extends to cover its claws.",
     "Plant eaters, they munch on shrubs and grasses",
     "10",
@@ -39,4 +39,37 @@ new Animal('Quokka',
     "Mammals"
 )]
 
-const birds = [new Animal('')];
+const birds = [new Animal('Cassowary',
+    "44kg",
+    "1.7cm",
+    "It’s not hard to imagine that cassowaries are descended from dinosaur ancestors. The largest cassowaries can stand as high as six feet and weigh up to 160 pounds.   These large birds cannot fly, but their extremely powerful legs propel them at great speeds. They are strong swimmers and can move quickly on both land and water. Cassowaries are shy and they are usually hard to spot, at least in their natural rain forest habitats. They are not overly aggressive, and attacks are rare. But they can do a lot of damage if they are provoked or angered. Cassowary attacks have occasionally been deadly, including a recent one which occurred in 2019, at a private collection of caged birds in Florida",
+    "Plants matter like fruit, insects and small animals like mice amd lizards",
+    "20",
+    "Queensland",
+    "Bird"
+ ),
+new Animal('Kookaburra',
+    "300g",
+    "43cm",
+    "Native to the eucalyptus forests of eastern Australia, the laughing kookaburra is the largest member of the Kingfisher family, with females weighing up to one pound and growing to 43 cm in length. Its beak can reach 10 cm long and is used to snatch a variety of invertebrates and small vertebrates, including the occasional small snake. Since being introduced in western Australia and New Zealand, the kookaburra has angered farmers by preying on their fowl. The laughing kookaburra has dark brown wing plumage and a white head and underside. Dark brown eye stripes run across its face and its upper bill is black. Its reddish-coloured tail is patterned with black bars.",
+    "Insects and smalll animals including snakes, frogs and lizards",
+    "20",
+    "Australia wide",
+    "Bird"
+),
+new Animal('Yellow Tailed Black Cockatoo',
+    "900g",
+    "65cm",
+    "It has a short crest on the top of its head. Its plumage is mostly brownish black and it has prominent yellow cheek patches and a yellow tail band. The body feathers are edged with yellow giving a scalloped appearance. The adult male has a black beak and pinkish-red eye-rings, and the female has a bone-coloured beak and grey eye-rings. In flight, yellow-tailed black cockatoos flap deeply and slowly, with a peculiar heavy fluid motion. Their loud, wailing calls carry for long distances. The whiteae is found south of Victoria to the East of South Australia and is smaller in size. The yellow-tailed black cockatoo is found in temperate forests and forested areas across south and central eastern Queensland to southeastern South Australia, including a very small population persisting in the Eyre Peninsula.[4] Two subspecies are recognised, although Tasmanian and southern mainland populations of the southern subspecies xanthanotus may be distinct enough from each other to bring the total to three. Birds of subspecies funereus (Queensland to eastern Victoria) have longer wings and tails and darker plumage overall, while those of xanthanotus (western Victoria, South Australia and Tasmania) have more prominent scalloping.",
+    "Fruit, seeds and other plant material",
+    "41",
+    "SE Australia",
+    "Bird"
+)]
+
+const reptiles = [new Animal('Frill-necked Lizard',
+    "1kg",
+    "90cm",
+    "When this unique creature feels threatened, it rises on its hind legs, opens its yellow-coloured mouth, unfurls the colorful, pleated skin flap that encircles its head, and hisses. If an attacker is unintimidated by these antics, the lizard simply turns tail, mouth and frill open, and bolts, legs splaying left and right. It continues its deliberate run without stopping or looking back until it reaches the safety of a tree.",
+    
+)]
