f40e296ffe02588c9383053f4a53bd53f499b4f4
bushra-rauf <bush_rauf@yahoo.com>
2025-02-13 14:29:03 +0100

    added loop

--- routes/birds.js
+++ routes/birds.js
@@ -1,42 +1,28 @@
 import express from "express";
-import {birds} from "../data/animals.js";
+import { birds } from "../data/animals.js";
 
 const birdsRouter = express.Router();
-
 const app = express();
-birdsRouter.get("/", (req,res) => {
-    res.render("pages/animal-page", {
-        headTitle:"Group-AssignmentNode",
-        title: "Birds", 
-        typePage: "home"
-    })
-})
 
-birdsRouter.get(`/${birds[0].name.trim().toLowerCase()}`,(req,res) => {
-    res.render("pages/animal-page", {
-        headTitle:"bird1",
-        title: birds[0].name,
-        animalData : birds[0],
-})
+birdsRouter.get("/", (req, res) => {
+  res.render("pages/animal-page", {
+    headTitle: "Group-AssignmentNode",
+    title: "Birds",
+    typePage: "home",
+    animalData: birds,
+  });
 });
 
-birdsRouter.get(`/${birds[1].name.trim().toLowerCase()}`,(req,res) => {
+birds.forEach((bird) => {
+  birdsRouter.get(`/${bird.name.trim().toLowerCase()}`, (req, res) => {
     res.render("pages/animal-page", {
-        headTitle:"bird2",
-        title: birds[1].name,
-        animalData : birds[1],
-})
+      headTitle: bird.name,
+      title: bird.name,
+      animalData: bird,
+    });
+  });
 });
-
-birdsRouter.get(`/${birds[2].name.trim().toLowerCase()}`,(req,res) => {
-    res.render("pages/animal-page", {
-        headTitle:"bird3",
-        title: birds[2].name,
-        animalData : birds[2],
-})
-});
-
 
 app.use("/birds", birdsRouter);
 
-export default birdsRouter
+export default birdsRouter;
