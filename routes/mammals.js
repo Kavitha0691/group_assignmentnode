db62591bd34b80547e0e3cf11e2766998f4abf30
kavitha0691 <kavitha0691@gmail.com>
2025-02-17 10:33:12 +0100

    added changes on mammals router

--- routes/mammals.js
+++ routes/mammals.js
@@ -2,38 +2,29 @@ import express from "express";
 import {mammals} from "../data/animals.js";
 
 const mammalsRouter = express.Router();
+const app = express();
 
 mammalsRouter.get("/", (req, res) => {
     res.render("pages/animal-page", {
-        headtitle: "Mammals",
-        title: "Mammals",
-        animalData: mammals,
-    }) 
- });
-
- mammalsRouter.get(`/${mammals[0].name.trim().toLowerCase()}`, (req, res) => {
-    res.render("pages/animal-page", {
-        headtitle: "Mammals",
-        title: mammals[0].name,
-        animalData: mammals[0],
-    }) 
- });
-
- mammalsRouter.get(`/${mammals[1].name.trim().toLowerCase()}`, (req, res) => {
-    res.render("pages/animal-page", {
-        headtitle: "Mammals",
-        title: mammals[1].name,
-        animalData: mammals[1],
-    }) 
- });
-
- mammalsRouter.get(`/${mammals[2].name.trim().toLowerCase()}`, (req, res) => {
-    res.render("pages/animal-page", {
-        headtitle: "Mammals",
-        title: mammals[2].name,
-        animalData: mammals[2],
-    }) 
- });
-
- export default mammalsRouter;
-
+      headTitle: "Group-AssignmentNode",
+      title: "mammals",
+      typePage: "home",
+      description: "Mammals are a group of animals that are found in the . They are a type of animal that can fly. They are a type of animal that can fly. They are a type of animal that can fly.",
+      animals: mammals,
+    });
+  });
+   
+  mammals.forEach((mammal) => {
+    mammalsRouter.get(`/${mammal.slug}`, (req, res) => {
+      res.render("pages/animal-page", {
+        headTitle: mammal.name,
+        title: mammal.name,
+        animals: mammals,
+        animalData: mammal,
+      });
+    });
+  });
+   
+  app.use("/mammals", mammalsRouter);
+   
+  export default mammalsRouter;
